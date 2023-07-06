503f2201fcbb5c07845778ab5b09e57d4f290cdc
Connor Skees <connor1skees@gmail.com>
2023-07-06 11:37:10 -0400

    resolve failing stickiness fuzz tests

--- packages/dds/sequence/src/intervalCollection.ts
+++ packages/dds/sequence/src/intervalCollection.ts
@@ -2731,6 +2731,8 @@ export class IntervalCollection<TInterval extends ISerializableInterval>
 					newStart,
 					interval.start.refType,
 					op,
+					undefined,
+					undefined,
 					startReferenceSlidingPreference(interval.stickiness),
 				);
 				if (props) {
@@ -2749,6 +2751,8 @@ export class IntervalCollection<TInterval extends ISerializableInterval>
 					newEnd,
 					interval.end.refType,
 					op,
+					undefined,
+					undefined,
 					endReferenceSlidingPreference(interval.stickiness),
 				);
 				if (props) {
